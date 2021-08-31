52a3797d73224b3fd2c68a348f71572f2c2bb5cd
ytkimirti <yusuftaha9@gmail.com>
2021-09-01 00:01:22 +0300

    Removed plugins bc of submodule thing

---
+++ custom/my_aliases.zsh
@@ -0,0 +1,13 @@
+alias coode='code ~/tmp'
+alias leet='code ~/.leetcode'
+#My experimental conda thing
+
+#fix this thing m8
+#Add conda to path
+alias condaset='export PATH="/Users/ytkimirti/opt/anaconda3/bin:$PATH"'
+#alias condaset='source $ZSH/condaset.zsh'
+#Remove conda from path
+alias condarm='export PATH=`echo $PATH | tr ":" "\n" | grep -v "anaconda" | tr "\n" ":"`'
+
+alias t='tldr'
+alias addalias='nvim ~/.oh-my-zsh/custom/my_aliases.zsh'
