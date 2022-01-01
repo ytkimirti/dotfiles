4128db673a1da2cf655d058947ad814acd8a634e
ytkimirti <yusuftaha9@gmail.com>
2022-01-01 14:57:18 +0300

    Autocommit

--- load.sh
+++ load.sh
@@ -1,4 +1,3 @@
-
 confirm() {
     # call with a prompt string or use a default
     read -r -p "${1:-Are you sure? [y/N]} " response
@@ -14,9 +13,12 @@ confirm() {
 
 confirm "This will overrite repo [y/N]"  || exit 0
 
-cp ~/.vimrc ~/dotfiles/.vimrc
-cp ~/.zshrc ~/dotfiles/.zshrc
-cp ~/.oh-my-zsh/custom/my_aliases.zsh ~/dotfiles/my_aliases.zsh
+for item in $dotfiles ; do
+	echo 'cp ~/$item ~/dotfiles/$item'
+done
+#cp ~/.vimrc ~/dotfiles/.vimrc
+#cp ~/.zshrc ~/dotfiles/.zshrc
+#cp ~/.oh-my-zsh/custom/my_aliases.zsh ~/dotfiles/my_aliases.zsh
 
 git add . && git commit -m "Autocommit"
 
@@ -28,4 +30,4 @@ yes '=' | head -n$(($COLUMNS)) | tr -d '\n'
 
 confirm "Push changes? [y/N]"  || exit 0
 
-
+git push
--- unload.sh
+++ unload.sh
@@ -1,3 +1,39 @@
+#dotfiles=".vimrc .zshrc .tmux.conf"
+
+
+exit 0
+
+confirm() {
+	# call with a prompt string or use a default
+	read -r -p "${1:-Are you sure? [y/N]} " response
+	case "$response" in
+		[yY][eE][sS]|[yY])
+			true
+			;;
+		*)
+			false
+			;;
+	esac
+
+}
+
+confirm "This will overrite repo [y/N]"  || exit 0
+
 cp ~/dotfiles/.vimrc ~/.vimrc 
 cp ~/dotfiles/.zshrc ~/.zshrc 
 cp ~/dotfiles/my_aliases.zsh ~/.oh-my-zsh/custom/my_aliases.zsh 
+
+git add . && git commit -m "Autocommit"
+
+yes '=' | head -n$(($COLUMNS)) | tr -d '\n'
+
+git --no-pager diff origin/master master
+
+yes '=' | head -n$(($COLUMNS)) | tr -d '\n'
+
+confirm "Push changes? [y/N]"  || exit 0
+
+git push
+
+
+
