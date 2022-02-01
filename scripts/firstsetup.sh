47df216e00626e31bd0917290a597f0dd1389524
Yusuf <yusuftaha9@gmail.com>
2022-02-01 14:07:25 +0300

    gitconfig remove from setup

--- scripts/firstsetup.sh
+++ scripts/firstsetup.sh
@@ -9,9 +9,4 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 git config --global url.ssh://git@github.com/.insteadOf https://github.com/
 
-git config --global user.name "Yusuf"
-git config --global user.email yusuftaha9@gmail.com
-git config --global diff.tool vimdiff
-git config --global difftool.prompt false
-
 git config --global core.excludesfile ~/dotfiles/gitignore_global
