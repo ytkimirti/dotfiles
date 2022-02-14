f2c1e1623670aa7dbf4127e75938a12df6c09bb8
Yusuf <yusuftaha9@gmail.com>
2022-02-14 15:08:18 +0300

    fix

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,3 +1,5 @@
 
 # Install brew
-cd /goinfre/$(whoami) && mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 2 -C homebrew
+#cd /goinfre/$(whoami) && mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 2 -C homebrew
+
+cd /goinfre/$(whoami) && mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
