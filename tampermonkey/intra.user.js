736d7a2456a876bc26406f706f35f32e661d52bd
Yusuf <yusuftaha9@gmail.com>
2022-04-01 18:05:03 +0300

    beutify

--- tampermonkey/intra.user.js
+++ tampermonkey/intra.user.js
@@ -10,124 +10,128 @@
 // Copy
 // ==/UserScript==
 
-
 function getProjectGitUrl(url, onComplete) {
-    console.log(`Getting ${url}`)
-    fetch(url, {
-        "headers": {
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "accept-language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7,fr;q=0.6",
-            "cache-control": "max-age=0",
-            "if-none-match": "W/\"86142865d583c56c820695657d459cc9\"",
-            "sec-ch-ua": "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"",
-            "sec-ch-ua-mobile": "?0",
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "same-site",
-            "sec-fetch-user": "?1",
-            "upgrade-insecure-requests": "1"
-        },
-        "referrer": "https://profile.intra.42.fr/",
-        "referrerPolicy": "strict-origin-when-cross-origin",
-        "body": null,
-        "method": "GET",
-        "mode": "cors",
-        "credentials": "include"
-    }).then(function (response) {
-        return response.text();
-    }).then(function (html) {
-        console.log(`Success getting ${url}`)
-        // SUCCESSS HERE
-        var parser = new DOMParser();
-        var htmlDoc = parser.parseFromString(html, "text/html")
+  console.log(`Getting ${url}`);
+  fetch(url, {
+    headers: {
+      accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "accept-language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7,fr;q=0.6",
+      "cache-control": "max-age=0",
+      "if-none-match": 'W/"86142865d583c56c820695657d459cc9"',
+      "sec-ch-ua":
+        '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-fetch-dest": "document",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "same-site",
+      "sec-fetch-user": "?1",
+      "upgrade-insecure-requests": "1",
+    },
+    referrer: "https://profile.intra.42.fr/",
+    referrerPolicy: "strict-origin-when-cross-origin",
+    body: null,
+    method: "GET",
+    mode: "cors",
+    credentials: "include",
+  })
+    .then(function (response) {
+      return response.text();
+    })
+    .then(function (html) {
+      console.log(`Success getting ${url}`);
+      // SUCCESSS HERE
+      var parser = new DOMParser();
+      var htmlDoc = parser.parseFromString(html, "text/html");
 
-        const gitUrlElement = htmlDoc.querySelector("div.team-repo > div > input")
-        if (!gitUrlElement)
-            return;
+      const gitUrlElement = htmlDoc.querySelector(
+        "div.team-repo > div > input"
+      );
+      if (!gitUrlElement) return;
 
-        console.log(gitUrlElement.value)
+      console.log(gitUrlElement.value);
 
-        // Execute the callback function
-        if (onComplete)
-            onComplete(gitUrlElement.value);
-
-    }).catch(function (err) {
-        // There was an error
-        console.warn(`Requesting ${url} unsuccessfull`, err);
+      // Execute the callback function
+      if (onComplete) onComplete(gitUrlElement.value);
+    })
+    .catch(function (err) {
+      // There was an error
+      console.warn(`Requesting ${url} unsuccessfull`, err);
     });
 }
 
 (function () {
-    'use strict';
+  "use strict";
 
-    $(function () {
+  $(function () {
+    console.log(window.location.href);
 
+    const url = new URL(window.location.href);
+    const command_name = url.searchParams.get("command");
 
-        console.log(window.location.href);
+    const url_base = url.hostname.toString().split(".")[0];
 
-        const url = new URL(window.location.href);
-        const command_name = url.searchParams.get("command");
+    if (url_base == "profile") {
+      const project_item = $(
+        "body > div.page > div.page-content.page-content-fluid > div > div.align-top > div > div.container-fullsize.full-width.fixed-height > div > div:nth-child(5) > div > div"
+      );
 
-        const url_base = url.hostname.toString().split('.')[0]
+      //project_item.children().css("background-color", "red")
+      const links = project_item.children();
 
-        if (url_base == "profile") {
+      const new_obj = $("<a>Heloo!</a>");
 
-            const project_item = $("body > div.page > div.page-content.page-content-fluid > div > div.align-top > div > div.container-fullsize.full-width.fixed-height > div > div:nth-child(5) > div > div")
+      // Copy git ID
+      links.append(function (index, str) {
+        const original_link = $(this).attr("href");
 
-            //project_item.children().css("background-color", "red")
-            const links = project_item.children()
+        const new_object = $(
+          `<a style="float: right; margin-left: 5px;">Copy URL</a>`
+        );
 
-            const new_obj = $("<a>Heloo!</a>")
+        const new_link = getProjectGitUrl(original_link, (gitUrl) => {
+          new_object.text("NAJS");
 
-            // Copy git ID
-            links.append(function (index, str) {
-                const original_link = $(this).attr("href")
+          new_object.click(function (event) {
+            navigator.clipboard.writeText(gitUrl);
+            event.preventDefault();
+            const original_text = $(this).text();
+            $(this).text("Copied!");
+            setTimeout(() => {
+              $(this).text(original_text);
+            }, 1000);
+          });
+        });
 
-                const new_object = $(`<a style="float: right; margin-left: 5px;">Copy URL</a>`)
+        return new_object;
+      });
+      // Subject link
+      links.append(function (index, str) {
+        const original_link = $(this).attr("href");
 
-                const new_link = getProjectGitUrl(original_link, (gitUrl) => {
-                    new_object.text("NAJS")
+        const new_link = `${original_link}?command=click_subject`;
 
-                    new_object.click(function (event) {
-                        navigator.clipboard.writeText(gitUrl);
-                        event.preventDefault()
-                        const original_text = $(this).text()
-                        $(this).text("Copied!")
-                        setTimeout(() => {
-                            $(this).text(original_text)
-                        }, 1000)
-                    })
-                });
+        return `<a style="float: right;" href="${new_link}">Subject</a>`;
+      });
 
-                return new_object
-            })
-            // Subject link
-            links.append(function (index, str) {
-                const original_link = $(this).attr("href")
+      // links.each(function (id, link) {
+      //     const new_element = $(`<p>Selam sekerim</p>`)
 
-                const new_link = `${original_link}?command=click_subject`
+      //     link.append(new_element)
+      // })
+      console.log(links);
+    } else if (url_base == "projects") {
+      if (command_name == "click_subject") {
+        const subject_link = $(
+          "#project-show > div.project-main > div.project-summary > div:nth-child(3) > div > div > h4 > a"
+        );
+        //subject_link.attr("target", "")
+        subject_link[0].click();
 
-                return (`<a style="float: right;" href="${new_link}">Subject</a>`)
-            })
-
-            // links.each(function (id, link) {
-            //     const new_element = $(`<p>Selam sekerim</p>`)
-
-            //     link.append(new_element)
-            // })
-            console.log(links)
-        }
-        else if (url_base == "projects") {
-            if (command_name == "click_subject") {
-
-                const subject_link = $("#project-show > div.project-main > div.project-summary > div:nth-child(3) > div > div > h4 > a")
-                //subject_link.attr("target", "")
-                subject_link[0].click()
-
-                $("body > div.main-navbar > div.main-navbar-left > div > a > img")[0].click()
-            }
-        }
-
-    })
-
+        $(
+          "body > div.main-navbar > div.main-navbar-left > div > a > img"
+        )[0].click();
+      }
+    }
+  });
 })();
