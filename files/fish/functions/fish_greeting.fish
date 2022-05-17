2d0a706b34c39803769b48fc3b2928fef6904a32
Yusuf <yusuftaha9@gmail.com>
2022-05-17 12:53:40 +0300

    Add fish files

---
+++ files/fish/functions/fish_greeting.fish
@@ -0,0 +1,13 @@
+function magical
+	echo "$(set_color magenta)(ﾉ◕ヮ◕)ﾉ\
+$(set_color yellow)*:･ﾟ✧ \
+$(set_color green)\
+$argv[1]\
+$(set_color yellow)\
+ ✧ﾟ･ *$(set_color magenta)\
+ヽ(◕ヮ◕ヽ)$(set_color normal)"
+end
+
+function fish_greeting
+	magical "Welcome"
+end
