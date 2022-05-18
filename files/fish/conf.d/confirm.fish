c269bf79673a8b0ffb20a0d77fb8a9a6caffc4e0
Yusuf <yusuftaha9@gmail.com>
2022-05-18 12:24:00 +0300

    Autocommit

---
+++ files/fish/conf.d/confirm.fish
@@ -0,0 +1,23 @@
+function read_confirm
+  while true
+    read -l -P "$(set_color cyan)\
+$argv[1] \
+$(set_color reset)\
+[\
+$(set_color green)\
+y\
+$(set_color reset)\
+/\
+$(set_color red)\
+N\
+$(set_color reset)\
+] " confirm
+
+    switch $confirm
+      case Y y
+        return 0
+      case '' N n
+        return 1
+    end
+  end
+end
