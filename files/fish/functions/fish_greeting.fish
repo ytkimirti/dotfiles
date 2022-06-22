a455592a92c75a156724418e8b82304b9c151c0c
Yusuf <yusuftaha9@gmail.com>
2022-06-22 23:42:28 +0300

    Stuff

--- files/fish/functions/fish_greeting.fish
+++ files/fish/functions/fish_greeting.fish
@@ -1,19 +1,9 @@
-function magical
-	echo "$(set_color magenta)(ﾉ◕ヮ◕)ﾉ\
-$(set_color yellow)*:･ﾟ✧ \
-$(set_color green)\
-$argv[1]\
-$(set_color yellow)\
- ✧ﾟ･ *$(set_color magenta)\
-ヽ(◕ヮ◕ヽ)$(set_color normal)"
-end
-
 function fish_greeting
+    return
 	set -l message \
-		   'Hoşgeldiniz efendim' \
-		   'Çay veriyim mi abim' \
-		   'Welcome' \
-		   'sa' \
-
-	magical $message[(random 1 (count $message))]
+        'Hoşgeldiniz efendim' \
+        'Çay veriyim mi abim' \
+        Welcome \
+        sa
+    magical $message[(random 1 (count $message))]
 end
