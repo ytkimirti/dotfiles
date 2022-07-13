96dc49eaef409b38f154870030860d81d93a6527
Yusuf <yusuftaha9@gmail.com>
2022-07-13 10:14:22 +0300

    nvchad defaults

---
+++ files/nvchad/chadrc.lua
@@ -0,0 +1,59 @@
+local M = {}
+
+--[[
+# Notes
+`lua/core/default_config.lua` has the structure
+
+
+--]]
+
+M.ui = {
+   theme = 'gruvchad'
+}
+
+M.plugins = {
+   -- Override init settings for stuff like
+   -- nvim-tree or treesitter
+   override = {
+
+   },
+
+   -- options = {
+   --    lspconfig = {
+   --       setup_lspconf = "custom.plugins.lspconfig"
+   --    }
+   -- }
+}
+
+
+return M
+
+-- local override = require "custom.override"
+
+
+-- M.plugins = {
+--
+--    options = {
+--       lspconfig = {
+--          setup_lspconf = "custom.plugins.lspconfig",
+--       },
+--    },
+--
+--    override = {
+--       ["kyazdani42/nvim-tree.lua"] = override.nvimtree,
+--       ["nvim-treesitter/nvim-treesitter"] = override.treesitter,
+--       ["lukas-reineke/indent-blankline.nvim"] = override.blankline,
+--    },
+--
+--    user = require "custom.plugins",
+-- }
+--
+-- M.ui = {
+--    theme = "gruvchad",
+--    hl_add = require "custom.highlights",
+--    theme_toggle = { "gruvchad", "gruvbox_light" },
+-- }
+--
+-- M.mappings = require "custom.mappings"
+--
+-- return M
---
+++ files/nvchad/plugins/init.lua
@@ -0,0 +1,53 @@
+return {
+   -- format & linting
+   -- ["jose-elias-alvarez/null-ls.nvim"] = {
+   --    after = "nvim-lspconfig",
+   --    config = function()
+   --       require "custom.plugins.null-ls"
+   --    end,
+   -- },
+
+   -- minimal modes
+   -- ["Pocco81/TrueZen.nvim"] = {
+   --    cmd = {
+   --       "TZAtaraxis",
+   --       "TZMinimalist",
+   --       "TZFocus",
+   --    },
+   --    config = function()
+   --       require "custom.plugins.truezen"
+   --    end,
+   -- },
+
+   -- notes stuff
+   -- ["nvim-neorg/neorg"] = {
+   --    ft = "norg",
+   --    after = "nvim-treesitter",
+   --    config = function()
+   --       require "custom.plugins.neorg"
+   --    end,
+   -- },
+
+   -- get highlight group under cursor
+   -- ["nvim-treesitter/playground"] = {
+   --    cmd = "TSCaptureUnderCursor",
+   --    config = function()
+   --       require("nvim-treesitter.configs").setup()
+   --    end,
+   -- },
+
+   -- dim inactive windows
+   -- ["andreadev-it/shade.nvim"] = {
+   --    module = "shade",
+   --    config = function()
+   --       require("custom.plugins.smolconfigs").shade()
+   --    end,
+   -- },
+
+   -- ["Pocco81/AutoSave.nvim"] = {
+   --    module = "autosave",
+   --    config = function()
+   --       require("custom.plugins.smolconfigs").autosave()
+   --    end,
+   -- },
+}
