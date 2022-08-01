18dbb9494b21ffb39d5693e27d3b40d145babd6b
Yusuf <yusuftaha9@gmail.com>
2022-08-02 00:59:28 +0300

    Add starship

--- files/fish/config.fish
+++ files/fish/config.fish
@@ -23,3 +23,7 @@ set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
 set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
 set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
 set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
+
+if type -q starship
+	starship init fish | source
+end
