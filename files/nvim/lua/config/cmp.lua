46932182cb89a511418e077f406571ab00c761cc
Yusuf <yusuftaha9@gmail.com>
2022-08-04 13:50:27 +0300

    Better cmp maps, now happier

--- files/nvim/lua/config/cmp.lua
+++ files/nvim/lua/config/cmp.lua
@@ -31,14 +31,14 @@ cmp.setup({
 		['<C-,>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			elseif has_words_before() then
-				cmp.complete()
+			-- elseif luasnip.expand_or_jumpable() then
+			-- 	luasnip.expand_or_jump()
+			-- elseif has_words_before() then
+			-- 	cmp.complete()
 			else
 				fallback()
 			end
