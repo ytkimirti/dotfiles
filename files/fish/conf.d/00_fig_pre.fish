7aff2e602c5133f56a0d846c7ac1f1ec9d649c57
Yusuf <yusuftaha9@gmail.com>
2022-08-05 17:53:54 +0300

    Change stuff

--- files/fish/conf.d/00_fig_pre.fish
+++ files/fish/conf.d/00_fig_pre.fish
@@ -1,1 +1,3 @@
-eval (~/.local/bin/fig init fish pre --rcfile 00_fig_pre | string split0)
+if [ -x ~/.local/bin/fig ]
+	eval (~/.local/bin/fig init fish pre --rcfile 00_fig_pre | string split0)
+end
