b94d618c7998f1024640fd5f33858939ad955991
Yusuf <yusuftaha9@gmail.com>
2022-10-26 12:26:00 +0300

    Remove some simple stuff

--- files/nvim/lua/config/cmp.lua
+++ files/nvim/lua/config/cmp.lua
@@ -1,14 +1,7 @@
 local cmp = require 'cmp'
 local luasnip = require("luasnip")
 local lspkind = require('lspkind')
-local set = vim.keymap.set
 
-local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-vim.cmd("inoremap <C-i> <nop>")
 cmp.setup({
 	formatting = {
 		format = lspkind.cmp_format(),
