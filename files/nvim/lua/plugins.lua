d74b995911979d57d61c006eced4db1fca5a0021
Yusuf <yusuftaha9@gmail.com>
2022-10-30 14:32:43 +0300

    Change auto install scripts

--- files/nvim/lua/plugins.lua
+++ files/nvim/lua/plugins.lua
@@ -1,23 +1,16 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-	print "Running the function"
-	PACKER_BOOTSTRAP = fn.system {
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	}
-	print("Function return value: " .. PACKER_BOOTSTRAP)
-	print "Installing packer close and reopen Neovim..."
-	vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
 
--- Use a protected call so we don't error out on first use
+local packer_bootstrap = ensure_packer()
+
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then return end
 
@@ -102,9 +95,7 @@ return packer.startup(function(use)
 	-- 	requires = { 'nvim-treesitter/nvim-treesitter' }
 	-- }
 
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
+	if packer_bootstrap then
+		require('packer').sync()
 	end
 end)
