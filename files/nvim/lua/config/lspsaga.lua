754a0a285bd2c74fdf7500309ec4da68bdb51d67
Yusuf <yusuftaha9@gmail.com>
2023-02-13 12:13:57 +0300

    fix saga

--- files/nvim/lua/config/lspsaga.lua
+++ files/nvim/lua/config/lspsaga.lua
@@ -1,16 +1,1 @@
-require('lspsaga').init_lsp_saga {
-	rename_action_quit = "<esc>",
-	finder_action_keys = {
-		open = "o",
-		vsplit = "s",
-		split = "i",
-		tabe = "t",
-		quit = { 'q', '<esc>' },
-		scroll_down = "<C-f>",
-		scroll_up = "<C-b>",
-	},
-	code_action_keys = {
-		quit = {"q", '<esc>'},
-		exec = "<CR>",
-	},
-}
+require('lspsaga').setup({})
--- files/nvim/lua/lspmaps.lua
+++ files/nvim/lua/lspmaps.lua
@@ -9,8 +9,6 @@ local set = vim.keymap.set
 local function saga_new()
 	local saga = require('lspsaga')
 
-	saga.init_lsp_saga()
-
 	-- Lsp finder find the symbol definition implement reference
 	-- if there is no implement it will hide
 	-- when you use action in finder like open vsplit then you can
